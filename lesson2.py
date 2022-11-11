df27da0514c35510d4a0122ff809e6cf6c1a7961
Aabesov <aabesov2005@mail.ru>
2022-11-11 11:38:39 +0600

    update lesson2

--- lesson2.py
+++ lesson2.py
@@ -1,31 +1,4 @@
 
-#print(str1 * 3)
-#orint(len(str()))
-#rint(len(str1()))
-#rint(str1.lstrip())
-#rint(str1,rstrip())
-#rint(dir(str1))
-
-
-# str = "Bylo cvrgilsy"
-# print(str.startswith("Bylo"))
-
-
-
-
-
-
-
-
-# def form_landau (K, M, T, N, P):
-#     L = int((K * M * T)/((N **2) * P))
-#     return L
-#
-# print(form_landau(970, 90, 170, 60, 60))
-# print(form_landau(120, 120, 170, 100, 120))
-
-
-
 
 
 
